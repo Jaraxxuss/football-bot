c89048b0c0a59f66e0f5f9c9c2e2ea2545acf3d5
Yauheni Rudziankou <evgenij.rudenkov@mail.ru>
2021-08-28 13:35:22 +0300

    feat: added telegram bot dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.5.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.21"
-    kotlin("plugin.spring") version "1.5.21"
+    val kotlinVersion = "1.5.21"
+    val kotlinPluginSpringVersion = "1.5.21"
+    val springBootVersion = "2.5.4"
+    val springDependencyManagementVersion = "1.0.11.RELEASE"
+
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version springDependencyManagementVersion
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinPluginSpringVersion
+    kotlin("kapt") version kotlinPluginSpringVersion
 }
 
 group = "com.itsm"
@@ -22,14 +28,23 @@ repositories {
 }
 
 dependencies {
+    val telegramBotsSpringBootStarterVer = "4.1.2"
+
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    
+    // telegram
+    implementation("org.telegram:telegrambots-spring-boot-starter:$telegramBotsSpringBootStarterVer")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
